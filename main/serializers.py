fc5249f4424d138b90692a7187050174431d4615
Danil Sheen <dan.80956@gmail.com>
2024-02-28 20:36:44 +0300

    added serializers

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
-from main.models import Category, Product
+from main.models import Category, Subcategory, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SerializerMethodField()
+
+    @staticmethod
+    def get_subcategories(category):
+        return SubcategorySerializer(Subcategory.objects.filter(category=category), many=True).data
+
     class Meta:
         model = Category
+        fields = '__all__'
+
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subcategory
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
         fields = '__all__'
