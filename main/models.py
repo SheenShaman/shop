095aaed978477a7e092664321fa2b7f967635928
Danil Sheen <dan.80956@gmail.com>
2024-03-02 14:38:36 +0300

    added Cart and CartItem

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 NULLABLE = {'null': True, 'blank': True}
 
 
@@ -41,13 +43,19 @@ class Product(models.Model):
 
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, verbose_name='Подкатегория')
 
-    # def save(self, **kwargs):
-    #     self.category = self.subcategory.category
-    #     super().save(**kwargs)
-
     def __str__(self):
         return f'{self.name}'
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
